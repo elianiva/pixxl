8565afb1801c123ebca34cffcb61b548f4bea28b
elianiva <git@elianiva.my.id>
2026-03-21 18:53:02 +0700

    feat: use bun instead of node

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { serve } from "@hono/node-server";
 
 const PORT = Number.parseInt(process.env.HONO_PORT || "3000", 10);
 
@@ -7,27 +6,7 @@ const app = new Hono();
 
 app.get("/", (c) => c.text("Hello from hono"));
 
-const server = serve(
-  {
-    fetch: app.fetch,
-    port: PORT,
-  },
-  (info) => {
-    console.log(`🚀 Server started at http://localhost:${info.port}`);
-  },
-);
-
-process.on("SIGINT", () => {
-  server.close();
-  process.exit(0);
-});
-
-process.on("SIGTERM", () => {
-  server.close((err) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    process.exit(0);
-  });
-});
+export default {
+  port: PORT,
+  fetch: app.fetch,
+};
